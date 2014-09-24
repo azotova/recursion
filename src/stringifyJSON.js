be05732073cf871b5762040c74d0889f66957130
Anastasia Zotova <azotova@gmail.com>
2014-09-24 19:55:30 +0400

    Base case defined for empty object and array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,4 +6,12 @@ var stringifyJSON = function(obj) {
   // your code goes here
   if (typeof obj == "number" || typeof obj == "boolean" || obj==null) return String(obj);
   if (typeof obj == "string")	return "\"" + obj +"\"";
+  if (Array.isArray(obj)==true && obj.length==0) return "[]";
+  var testForEmptyObj = function(x) {
+  	var len=0;
+    for (var key in x) len=len+1;
+    if (len==0) return true;
+    return false;
+  };
+  if (typeof obj=="object" && testForEmptyObj(obj)==true) return "{}";
 };
